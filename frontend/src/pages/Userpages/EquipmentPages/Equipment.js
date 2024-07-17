0ce5d2f3fa96d05ab4217a60599aba1a36e95a39
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-07-17 23:44:46 +0530

    updated

--- frontend/src/pages/Userpages/EquipmentPages/Equipment.js
+++ frontend/src/pages/Userpages/EquipmentPages/Equipment.js
@@ -54,7 +54,7 @@ const Equipment = () => {
 
       <div style={containerStyle} className="bg-black bg-opacity-50 text-center p-4">
         <div className="text-white bg-opacity-70 rounded-md absolute left-40 bottom-52">
-          <h1 className="font-bold mb-4 text-6xl">Equipment Maintenance</h1>
+          <h1 className="font-bold mb-4 text-6xl">Equipment Maintenances</h1>
           <h2 className="text-2xl font-bold text-white mb-24">
             We are a team of <span className="text-green-500">Jlcniyon Service</span>
           </h2>
