1a60e4ebc7e4820964dfc97f5fa673867b6217a2
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-08-01 00:26:17 +0530

    add

--- frontend/src/pages/AdminPages/EquipmentMaintenance/EquipmentCard.js
+++ frontend/src/pages/AdminPages/EquipmentMaintenance/EquipmentCard.js
@@ -10,7 +10,7 @@ export default function EquipmentCard() {
 
       <Box className="flex-1 p-6 relative bottom-24 left-32">
         <Box className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          <Box className="p-6 bg-slate-50  hover:bg-lime-100 rounded-3xl shadow-md flex  border-x-4 border-green-700 items-center space-x-4">
+          <Box className="p-6    rounded-3xl shadow-md flex  border-x-4 border-green-700 items-center space-x-4">
             
             <a href="/Schedule_Maintenance" className="flex items-center space-x-2">
               <span  className="text-yellow-500 mr-0 w-16 h-16 flex  justify-center"
