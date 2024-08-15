40638d9264bfbb29c9df9453ad6895a4b1a8c366
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-08-16 01:07:30 +0530

    Home Page Updated

--- frontend/src/pages/Userpages/Home.js
+++ frontend/src/pages/Userpages/Home.js
@@ -1,96 +1,96 @@
-import React, { useState, useEffect, useRef } from 'react';
-import NavbarComponent from '../../components/Navigation_bar/User/NavbarComponent';
-import Footer from '../../components/footer/Footer';
-import Product1 from '../../assets/type1.png';
-import Product2 from '../../assets/type2.png';
-import Product3 from '../../assets/type3.png';
-import Tea_Grown from '../../assets/logo.png';
-import Alert from '../../components/Alert/Alert';
-import About from './Home_content';
-import Contact from '../../components/Contacts/Contacts';
-import imge1 from '../../assets/imge1.jpg';
-import imge2 from '../../assets/imge2.jpg';
-import imge3 from '../../assets/imge3.jpg';
-import imge4 from '../../assets/imge4.jpg';
+import React, { useState, useEffect, useRef } from "react";
+import NavbarComponent from "../../components/Navigation_bar/User/NavbarComponent";
+import Footer from "../../components/footer/Footer";
+import Product1 from "../../assets/type1.png";
+import Product2 from "../../assets/type2.png";
+import Product3 from "../../assets/type3.png";
+import Product4 from "../../assets/type3.png";
+import Alert from "../../components/Alert/Alert";
+import About from "./Home_content";
+import Contact from "../../components/Contacts/Contacts";
+import imge1 from "../../assets/imge1.jpg";
+import imge2 from "../../assets/imge2.jpg";
+import imge3 from "../../assets/imge3.jpg";
+import imge4 from "../../assets/imge4.jpg";
+import equipmentImage from '../../assets/Equipment/machine.png';
+import { MdMargin } from "react-icons/md";
 
-// Define images and slides arrays
 const images = [imge1, imge2, imge3, imge4];
-const slides = []; // Add your slide data here
-
 const services = [
-  { title: 'Production Management', icon: '🏭', color: 'bg-stone-900' },
-  { title: 'Quality Control', icon: '🔍', color: 'bg-green-500' },
-  { title: 'Packaging', icon: '📦', color: 'bg-stone-900' },
-  { title: 'Supplier Services', icon: '🚚', color: 'bg-green-500' },
-  { title: 'Sales', icon: '💼', color: 'bg-stone-900' },
-  { title: 'Customer Service', icon: '📞', color: 'bg-green-500' },
+  { title: "Production Management", icon: "🏭", color: "bg-stone-900", },
+  { title: "Quality Control", icon: "🔍", color: "bg-green-500" },
+  { title: "Packaging", icon: "📦", color: "bg-stone-900" },
+  { title: "Supplier Services", icon: "🚚", color: "bg-green-500" },
+  { title: "Sales", icon: "💼", color: "bg-stone-900" },
+  { title: "Customer Service", icon: "📞", color: "bg-green-500" },
 ];
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentSlide, setCurrentSlide] = useState(0);
   const formRef = useRef(null);
 
   const openForm = () => setIsOpen(true);
   const closeForm = () => setIsOpen(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    }, 5000);
+    if (images.length > 1) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 5000);
 
-    const handleClickOutside = event => {
+      return () => clearInterval(interval);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
       if (formRef.current && !formRef.current.contains(event.target)) {
         closeForm();
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      clearInterval(interval);
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide(prevSlide => (prevSlide + 1) % slides.length);
-    }, 4000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    
-    <div className="bg-white h-auto">
-      
-      
+    <div className="bg-white">
       {/* Slideshow */}
-      <div className="min-h-screen flex flex-col">
-        <div className="flex-grow relative">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className={`absolute inset-0 transition-opacity duration-1000 ${
-                index === currentIndex ? 'opacity-100' : 'opacity-0'
-              }`}
-            >
-              <img src={image} alt={`Slide ${index}`} className="w-full h-full object-cover brightness-50" />
-              <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-                <p className="text-sm tracking-widest uppercase">Importer and purveyor of fine tea since 1843</p>
-                <h1 className="mt-4 text-4xl font-bold md:text-5xl">Quality tea production from Sri Lanka</h1>
-                <a
-                  href="#aboutSection"
-                  className="mt-8 px-14 py-2 border border-green-500 text-green-500 inline-block hover:border-red-500 hover:text-red-500"
-                >
-                  About
-                </a>
-              </div>
+      <div className="min-h-screen relative flex flex-col">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`absolute inset-0 transition-opacity duration-1000 ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="w-full h-full object-cover brightness-50"
+            />
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+              <p className="text-sm tracking-widest uppercase">
+                Importer and purveyor of fine tea since 1843
+              </p>
+              <h1 className="mt-4 text-4xl font-bold md:text-5xl">
+                Quality tea production from Sri Lanka
+              </h1>
+              <a
+                href="#aboutSection"
+                className="mt-8 px-14 py-2 border border-green-500 text-green-500 inline-block hover:border-red-500 hover:text-red-500"
+              >
+                About
+              </a>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
       {/* Slideshow End */}
 
@@ -98,48 +98,90 @@ function Home() {
       <Alert />
       <NavbarComponent />
 
-      <div id="areaSection">
-        <div className="relative inset-0 flex flex-col items-center justify-center text-black">
-          <div className="text-slider"></div>
-        </div>
-        <div className="relative inset-0 flex flex-col items-center justify-center text-black top-24"></div>
-      </div>
-
-      <div className="font-sans">
-        <Contact isOpen={isOpen} closeForm={closeForm} />
-      </div>
-
       {/* Products */}
-      <h2 className="text-green-600 text-sm font-semibold uppercase relative top-3 text-center">From the Shop</h2>
-      <h1 className="text-5xl font-extrabold mb-2 text-black text-center relative bottom-16">Choose Your Taste</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-7/12 mx-auto h-auto rounded-t-2xl border-white rounded-bl-full">
-        {[
-          { src: Product3, name: 'Rosemary Black', price: '$9.00' },
-          { src: Product1, name: 'Indian Summer', price: '$9.00' },
-          { src: Product2, name: 'Fruit Rhapsody', price: '$9.00' },
-          { src: Product3, name: 'Chicory Roasted', price: '$9.00' },
-        ].map((product, index) => (
-          <div
-            key={index}
-            className="text-center relative group mb-4 sm:mb-0 flex flex-col items-center shadow-sm drop-shadow-lg w-full mx-auto h-auto max-w-xs rounded-full overflow-hidden"
-          >
-            <img
-              src={product.src}
-              alt={product.name}
-              className="w-40 h-40 object-cover mx-auto transition-transform duration-300 transform group-hover:scale-105 rounded-full"
-            />
-            <div className="mt-2 text-center">
-              <h2 className="text-xl font-bold mb-2">{product.name}</h2>
-              <p className="text-gray-600 mb-2">Donec fermentum nunc nenunc rutrum.</p>
-              <p className="text-green-500 font-semibold">{product.price}</p>
-              <button className="mt-11 px-4 py-1 w-24 h-10 bg-green-600 text-white rounded relative bottom-5 text-xs">
-                Add to Cart
-              </button>
+      <div className="relative bottom-64">
+        <h1 className="text-5xl font-extrabold mb-2 text-green-900 text-center mt-60">
+          Shop Our Teas
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 w-10/12 mx-auto mt-8">
+          {[
+            { src: Product1, name: "Chamomile Botanical Blend Sachets", price: "$4.00–$28.00", discount: "30% OFF" },
+            { src: Product2, name: "Organic Dandelion & Peach Naturally", price: "$3.00–$21.00", discount: "30% OFF" },
+            { src: Product3, name: "Chamomile Blend Organic Tea", price: "$5.00–$35.00", discount: "30% OFF" },
+            { src: Product4, name: "Chamomile Botanical Blend Sachets", price: "$20.00–$140.00", discount: "30% OFF" },
+            // Repeat the product items as needed
+          ].map((product, index) => (
+            <div
+              key={index}
+              className="text-center group mb-4 flex flex-col items-center shadow-lg w-full max-w-xs bg-white rounded-lg overflow-hidden"
+            >
+              <div className="relative">
+                <span className="absolute top-0 left-0 bg-red-500 text-white px-2 py-1 text-xs font-bold">
+                  {product.discount}
+                </span>
+                <img
+                  src={product.src}
+                  alt={product.name}
+                  className="w-full h-56 object-cover transition-transform duration-300 transform group-hover:scale-105"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-xl font-bold mb-2">{product.name}</h2>
+                <p className="text-gray-600 mb-2">{product.price}</p>
+                <button className="mt-4 px-4 py-2 w-28 bg-green-600 text-white rounded-full hover:bg-green-700">
+                  Add to Cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       {/* Products End */}
+
+      {/* Services */}
+      <div className="mx-auto bg-gradient-to-r relative bottom-44">
+        <div className="text-center mb-12 bg-lime-700 py-8">
+          <h2 className="text-green-300 text-lg font-bold uppercase tracking-wide">
+            What We Offer
+          </h2>
+          <h1 className="text-4xl font-extrabold text-white mt-2 mb-4">
+            Our Services
+          </h1>
+          <p className="text-white text-lg max-w-3xl mx-auto">
+            We offer a comprehensive range of services to ensure the smooth and
+            efficient operation of your tea factory. From production management
+            to customer service, we cover all aspects of tea factory management
+            with expertise and dedication.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-5xl mx-auto">
+          {services.map((service) => (
+            <div
+              key={service.title}
+              className={`p-8 rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl`}
+              style={{
+                background: `linear-gradient(145deg, #ffffff, #f0f0f0)`,
+                boxShadow: `6px 6px 12px #e0e0e0, -6px -6px 12px #ffffff`,
+              }}
+            >
+              <div
+                className={`rounded-full p-4 mb-6 ${service.color}`}
+              >
+                <span className="text-5xl text-white">{service.icon}</span>
+              </div>
+              <h3 className="text-2xl font-semibold text-gray-800 text-center mb-2">
+                {service.title}
+              </h3>
+              <p className="text-gray-600 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                auctor nisi eget nisl interdum.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Services End */}
 
       {/* Contact Button */}
       <button
@@ -150,29 +192,63 @@ function Home() {
       </button>
       {/* Contact Button End */}
 
+      {/* Additional Section */}
+      <div className="text-center  bg-yellow-700 py-8">
+          <h2 className="text-green-300 text-lg font-bold uppercase tracking-wide">
+            What We Offer
+          </h2>
+          <h1 className="text-4xl font-extrabold text-white mt-2 mb-4">
+            Our Services
+          </h1>
+          <p className="text-white text-lg max-w-3xl mx-auto">
+            We offer a comprehensive range of services to ensure the smooth and
+            efficient operation of your tea factory. From production management
+            to customer service, we cover all aspects of tea factory management
+            with expertise and dedication.
+          </p>
+        </div>
+      <div className="relative p-8 bg-gray-100 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="p-8">
+       
+          <div className="mb-4 p-4 bg-yellow-500 text-white rounded-md hover:scale-105">
+            <h3 className="text-2xl font-semibold">Equipment Maintenance</h3>
+            <p>
+              We provide a wide range of equipment essential for tea production,
+              including machinery for plucking, withering, rolling, and drying.
+              Our catalog features the latest technology to enhance efficiency
+              and quality.
+            </p>
+          </div>
+          <div className="p-4 bg-gray-800 text-white rounded-md mb-4 hover:scale-105">
+            <h3 className="text-2xl font-semibold">
+              Flexible Maintenance Plans
+            </h3>
+            <p>
+              Our flexible maintenance plans are designed to fit your schedule
+              and budget. Whether you need regular servicing or emergency
+              repairs, our skilled technicians are ready to keep your equipment
+              in top condition.
+            </p>
+          </div>
+          <div className="mb-4 p-4 bg-yellow-500 text-white rounded-md hover:scale-105">
+            <h3 className="text-2xl font-semibold">Expert Technical Support</h3>
+            <p>
+              Our team of experts offers technical support to troubleshoot and
+              resolve any equipment issues promptly. We ensure minimal downtime,
+              so your tea production process remains uninterrupted.
+            </p>
+          </div>
+        </div>
 
-      {/* Services */}
-      
-      <div className="max-w-4xl mx-auto px-4 py-12">
-        <h2 className="text-green-600 text-lg">What We Offer</h2>
-        <h1 className="text-4xl mb-4 font-extrabold">Our Services</h1>
-        <p className="mb-8 text-gray-600 font-serif">
-          We offer a comprehensive range of services to ensure the smooth and efficient operation of your tea factory. From production management to customer service, we cover all aspects of tea factory management with expertise and dedication.
-        </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {services.map(service => (
-            <div
-              key={service.title}
-              className={`${service.color} p-6 rounded-lg flex flex-col items-center hover:scale-105`}
-            >
-              <span className="text-5xl text-white">{service.icon}</span>
-              <h3 className="text-xl font-semibold mt-4 text-white">{service.title}</h3>
-            </div>
-          ))}
+        <div className="relative right-5">
+          <img
+            src={equipmentImage}
+            alt="Equipment"
+            className="object-cover w-full h-full rounded-lg"
+          />
         </div>
       </div>
-      {/* Services End */}
-     
+
       <Footer />
     </div>
   );
--- frontend/src/pages/Userpages/Home_content.js
+++ frontend/src/pages/Userpages/Home_content.js
@@ -10,6 +10,7 @@ import Product1 from "../../assets/tea1.png";
 import Product2 from "../../assets/tea2.png";
 import Product3 from "../../assets/tea3.png";
 import AboutContent from "./AboutContent";
+import equipmentImage from "../../assets/Equipment/machine.png";
 
 const teaCategories = [
   {
@@ -61,103 +62,40 @@ const Home_content = () => {
   }, [showModal]);
 
   return (
-    <section id="aboutSection" className="p-4">
-      <div className="relative bottom-28">
-        {/* First part */}
-
-        <div className="w-7/12 mx-auto h-auto rounded-t-2xl border-white rounded-bl-full">
-          <h2 className="text-green-600  text-4xl font-extrabold mb-2 uppercase text-center mt-10 animate-bounce relative top-36 left-2">
-            Soba Tea
+    <section id="aboutSection">
+      <div className="relative bottom-6 bg-green-900 text-white">
+        <img
+          src={Main1}
+          alt="Tea Leaves"
+          className="w-44 mb-4 relative left-1/2 transform -translate-x-1/2 top-14"
+        />
+        <div className="w-7/12 mx-auto h-auto py-16 mb-14">
+          <h2 className="text-white text-4xl font-extrabold mb-6 uppercase text-center">
+            Welcome to Our Tea Store
           </h2>
-
-          <div className="flex flex-col sm:flex-row items-center relative mt-10 sm:mt-20">
-            <img
-              src={Main1}
-              alt="Tea Leaves"
-              className="w-full sm:w-1/3 object-cover mb-4 sm:mb-0 skew-x-12"
-            />
-            <div className="p-8 text-center sm:text-left">
-              <h2 className="text-green-600 text-sm font-semibold uppercase mb-2 ml-10">
-                Featured Article
-              </h2>
-              <h1 className="text-4xl  text-black font-extrabold">
-                The history of tea leaf in the world
-              </h1>
-              <hr className="border-t-2 border-green-500 w-1/4 mb-4 mx-auto sm:mx-0" />
-              <p className="text-gray-700 mb-4 font-serif">
-                Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
-                Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
-                sed stet lorem sit clita duo justo magna dolore erat amet.
-              </p>
-              <p className="text-gray-700 mb-6 font-serif">
-                Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita
-                erat ipsum et lorem et sit, sed stet lorem sit clita duo justo
-                magna. Tempor erat elitr rebum at clita.
-              </p>
-              <button
-                className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600"
-                onClick={handleClick}
-              >
-                Read More
-              </button>
-            </div>
+          <p className="text-center mt-4 text-lg">
+            We are a family-run company founded on a passion for tea. Our
+            Chairman made a promise to never sell tea he wouldn’t drink at home.
+            Learn how we stay true to those values in everything we do.
+          </p>
+          <div className="text-center mt-8">
+            <button
+              className="bg-green-600 text-white px-6 py-2 rounded-full hover:bg-green-700"
+              onClick={handleClick}
+            >
+              Learn More
+            </button>
           </div>
-          {/* First part end */}
-
-          {/* Second part */}
-          <div className="mt-20 border-green-500 border-l-4 bg-green50">
-            <div className="max-w-6xl mx-auto p-8">
-              <h2 className="text-green-600 text-sm font-extrabold uppercase text-center mb-2">
-                Categories
-              </h2>
-
-              <h1 className="  text-center mb-6 text-4xl font-extrabold">
-                Choose Your Tea
-              </h1>
-              <hr className="border-t-2 border-green-500 w-1/4 mx-auto mb-6" />
-              <div className="flex flex-col sm:flex-row justify-around ">
-                {teaCategories.map((product, index) => (
-                  <div
-                    key={index}
-                    className="relative group mb-4 sm:mb-0 w-1/3 px-2 text-center "
-                  >
-                    <img
-                      src={product.src}
-                      alt={product.name}
-                      className="w-full h-auto block transition-transform duration-300 transform  hover:scale-11 "
-                    />
-                    <h3 className="text-xl font-semibold mt-4">
-                      {product.name}
-                    </h3>
-                    <p className="text-gray-500">{product.description}</p>
-                   
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          {/* Second part end */}
         </div>
       </div>
 
       {/* AboutContent Popup */}
-      {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div
-            className="bg-white rounded-lg p-8 mx-4 max-w-4xl relative"
-            ref={modalRef}
-          >
-            <AboutContent />
-          </div>
-          <button
-            className="absolute top-2 right-2 text-white rounded-full px-3 py-1"
-            onClick={closeModal}
-          >
-            Close
-          </button>
-        </div>
-      )}
+
       {/* AboutContent Popup End */}
+
+   
+
+     
     </section>
   );
 };
