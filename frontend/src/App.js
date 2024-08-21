2018fa0e78f1e718d7bdcaf0b180d7d444d9eb16
Chethiya07 <chethiyashehan6@gmail.com>
2024-08-21 15:20:47 +0530

    file make the qulity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import EquipmentCard from './pages/AdminPages/EquipmentMaintenance/EquipmentCard
 // import EmployeeManagement from './pages/AdminPages/Employee_management/EmployeeManagement';
 import ScheduleMaintenanceshow from './pages/AdminPages/EquipmentMaintenance/Schedule_Maintenance/ScheduleMaintenance';
 import Issue_Maintaining from './pages/AdminPages/EquipmentMaintenance/IssueMaintaining/Issue_Maintaining';
+import Quality_controller from './pages/AdminPages/Quality_controller/Quality_controller';
 // import Resources from './pages/AdminPages/EquipmentMaintenance/Resource/Resource';
 import MinePayment from './pages/AdminPages/EquipmentMaintenance/MinePayment';
 
@@ -47,6 +48,7 @@ function App() {
                 {/* <Route path="/Resources" element={<Resources />} /> */}
                 <Route path="/MinePayment" element={<MinePayment />} />
                 <Route path="/adminhome" element={<AdminHome />} />
+                <Route path="/Quality_controller" element={<Quality_controller />} />
                 {/* <Route path="/Employee_Management" element={<EmployeeManagement />} /> */}
             </Routes>
         </Router>
