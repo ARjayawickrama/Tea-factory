c182d192b40ac80826d7171312d1025fbbae5d9f
Chethiya07 <chethiyashehan6@gmail.com>
2024-08-21 14:20:12 +0530

    fixs

--- frontend/src/pages/AdminPages/Quality_controller/Quality_controller.js
+++ frontend/src/pages/AdminPages/Quality_controller/Quality_controller.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Quality_controller() {
   return (
     <div>
-      <h1>Quality_controllernkjbhdkjfvjkdbvbdvbdbv</h1>
+      <h1>Quality_controllernkjbhdkjfvjkdbvbdvbd</h1>
     </div>
   )
 }
