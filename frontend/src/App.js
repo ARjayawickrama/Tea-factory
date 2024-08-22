4cf50fc0a327fe62d3d5594900d03999469b0318
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-08-22 10:38:29 +0530

    Revert "Merge pull request #3 from ARjayawickrama/Equipment_test"
    This reverts commit 3c8c9f0d44d15c4408196209de13c73848e05b99, reversing
    changes made to add441e63716287febdb9f11b69575f0718cdeac.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Userpages/Home';
-import Login from './components/user_management/Login'; 
-import Register from './components/user_management/Registration'; 
-import AdminHome from './pages/AdminPages/AdminHome'; 
-
-import Equipment from './pages/Userpages/EquipmentPages/Equipment';
+import Login from './components/user_management/Login';
+import Register from './components/user_management/Registration';
+import AdminHome from './pages/AdminPages/AdminHome';
+import UserHome from './pages/Userpages/Home';
 import AboutContent from './pages/Userpages/AboutContent';
+import UsersList from './pages/AdminPages/Usermanagement';
 import Gallery from './pages/Userpages/Gallery';
-import EquipmentForm from './components/equipment_maintenance/EquipmentForm';
-import EquipmentDisplay from './components/equipment_maintenance/EquipmentDisplay';
-import EquipmentUpdate from './components/equipment_maintenance/EquipmentUpdate';
 import Usermanagement from './pages/AdminPages/Usermanagement';
-import EquipmentNav from './pages/AdminPages/EquipmentMaintenance/EquipmentNav';
-import EquipmentHome from './pages/AdminPages/EquipmentHome';
+
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/AboutContent" element={<AboutContent/>} />
+                <Route path="/AboutContent" element={<AboutContent />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register/>} />
-          
-                <Route path="/Gallery" element={<Gallery/>} />
-                <Route path="/equipment" element={<Equipment />} />
-                <Route path="/equipmentform" element={<EquipmentForm />} />
-                <Route path="/EquipmentDisplay" element={<EquipmentDisplay />} />
-                <Route path="/EquipmentUpdate/:id" element={<EquipmentUpdate />} />
-                <Route path="/EquipmentNav" element={<EquipmentNav />} />
-                <Route path="/equipmentmaintenancemanagement" element={<EquipmentHome />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/UsersList" element={<UsersList />} />
+                <Route path="/gallery" element={<Gallery />} />
                 <Route path="/usermanagement" element={<Usermanagement />} />
                 <Route path="/adminhome" element={<AdminHome />} />
+                <Route path="/userhome" element={<UserHome />} />
             </Routes>
         </Router>
     );
