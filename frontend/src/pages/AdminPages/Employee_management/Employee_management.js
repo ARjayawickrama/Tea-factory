10a7db91686512354db6031929d52ba8e0d25441
ARIYASINGHE A.H.T.P.S <157916124+SanadiAriyasighe@users.noreply.github.com>
2024-08-26 15:32:30 +0530

    isSidebar add

--- frontend/src/pages/AdminPages/Employee_management/Employee_management.js
+++ frontend/src/pages/AdminPages/Employee_management/Employee_management.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaUsers } from 'react-icons/fa';
 
-export default function Employee_management() {
+export default function EmployeeManagement() {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div>
-     <h1>Employee Management</h1>
-     
-     <h1>Employee Management</h1>
-     
-     <h1>Employee Management</h1>
-     
-     <h1>Employee Management</h1>
+    <div className="flex min-h-screen">
+
+      <div
+        className={`fixed top-0 left-0 h-full bg-stone-800 text-white w-64 transition-transform duration-300 ${
+          isSidebarOpen ? 'translate-x-0' : '-translate-x-64'
+        }`}
+      >
+        <nav>
+          <ul>
+            <li className="p-4 cursor-pointer bg-teal-500 mt-9 flex items-center">
+              <FaUsers className="w-8 h-8 mr-4" />
+              <span>EmployeeManagement</span> 
+            </li>
+           
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
