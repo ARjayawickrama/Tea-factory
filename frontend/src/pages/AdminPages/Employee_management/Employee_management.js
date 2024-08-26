d626bc404e0fb806fbe049dfb1bd751d94c157a7
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-08-26 14:47:26 +0530

    Update Employee_management.js

--- frontend/src/pages/AdminPages/Employee_management/Employee_management.js
+++ frontend/src/pages/AdminPages/Employee_management/Employee_management.js
@@ -4,6 +4,12 @@ export default function Employee_management() {
   return (
     <div>
      <h1>Employee Management</h1>
+     
+     <h1>Employee Management</h1>
+     
+     <h1>Employee Management</h1>
+     
+     <h1>Employee Management</h1>
     </div>
   )
 }
