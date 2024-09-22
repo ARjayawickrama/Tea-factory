5276141b07a7a3fccf9b3251e25377130f063087
Ramesh Jayawickrama <jayawickramaramesh@gmail.com>
2024-09-22 10:58:49 +0530

    add

--- frontend/src/pages/AdminPages/Employee_management/Employee_management.js
+++ frontend/src/pages/AdminPages/Employee_management/Employee_management.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { FaUsers } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import EmployeeList from '../Employee_Management/EmployeeList';
-import './EmployeeManagement.css'; // Import the CSS file
+import React, { useState } from "react";
+import { FaUsers } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import EmployeeList from "../Employee_Management/EmployeeList";
+import "./EmployeeManagement.css"; // Import the CSS file
 
 export default function EmployeeManagement() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -12,38 +12,34 @@ export default function EmployeeManagement() {
     <div className="">
       <div
         className={`fixed top-0 left-0 h-full bg-stone-800 text-white w-64 transition-transform duration-300 ${
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-64'
+          isSidebarOpen ? "translate-x-0" : "-translate-x-64"
         }`}
       >
         <nav>
           <ul>
-          <li className="p-4 mt-9 flex items-center">
-                        <button className="w-full flex items-center bg-amber-500 p-4 rounded">
-                        <FaUsers className="w-6 h-6 mr-4" />
-                        <span>Employee Management</span>
-                        </button>
-          </li>  
-
+            <li className="p-4 mt-9 flex items-center">
+              <button className="w-full flex items-center bg-amber-500 p-4 rounded">
+                <FaUsers className="w-6 h-6 mr-4" />
+                <span>Employee Management</span>
+              </button>
+            </li>
           </ul>
         </nav>
       </div>
 
-      <main className='relative right-30'>
-      <div className='grid grid-cols-2 gap-4 mt-8 relative left-72'>
-        <div className=' absolute left-80'>
-          <div className='bg-green-800 w-64 h-28 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg'>
-          Equipment Alerts
-         </div>
-       </div>
-        <div className='bg-green-800 w-64 h-28 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg'>
-    Alerts
-       </div>
-</div>
-
-
+      <main className="relative right-30">
+        <div className="grid grid-cols-2 gap-4 mt-8 relative left-72">
+          <div className=" absolute left-80">
+            <div className="bg-green-800 w-64 h-28 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg">
+              Equipment Alerts
+            </div>
+          </div>
+          <div className="bg-green-800 w-64 h-28 flex items-center justify-center text-white text-xl font-bold shadow-lg rounded-lg">
+            Alerts
+          </div>
+        </div>
 
         <EmployeeList />
-        
       </main>
     </div>
   );
