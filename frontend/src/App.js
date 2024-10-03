d1937ff9ff69c462524e38dd657453634dcd44e2
Shenith Madurawala <it22300096@my.sliit.lk>
2024-10-03 22:14:08 +0530

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,11 +51,11 @@ import QualityControllerManeger from "./pages/AdminPages/Quality_controller/Qual
 import Quality_supervisor from "./pages/AdminPages/Quality_controller/Quality_supervisor";
 import TeaIssueDisplay from "./pages/AdminPages/Quality_controller/TeaIssueDesply";
 // Employee Management
-import EmployeeList from "./pages/AdminPages/Employee_management/EmployeeList";
-import EmployeeAttendance from "./pages/AdminPages/Employee_management/EmployeeAttendance";
-import AddEmployeeForm from "./pages/AdminPages/Employee_management/AddEmployeeForm";
-import Employee_management from "./pages/AdminPages/Employee_management/Employee_management"; 
-import EmployeeSalaryDetails from "./pages/AdminPages/Employee_management/EmployeeSalaryDetails"; 
+import EmployeeList from "./pages/AdminPages/Employee_Management/EmployeeList";
+import EmployeeAttendance from "./pages/AdminPages/Employee_Management/EmployeeAttendance";
+import AddEmployeeForm from "./pages/AdminPages/Employee_Management/AddEmployeeForm";
+import Employee_management from "./pages/AdminPages/Employee_Management/Employee_management"; 
+import EmployeeSalaryDetails from "./pages/AdminPages/Employee_Management/EmployeeSalaryDetails"; 
 
 // Feedback
 import Feedback from "./pages/AdminPages/Feedback/Feedback";
