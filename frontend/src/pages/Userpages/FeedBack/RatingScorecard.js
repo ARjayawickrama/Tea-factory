2e30ad92953fff98acdc226eb4cfe89388350757
Tharusha Liyanage <147168826+Tharusha629@users.noreply.github.com>
2024-10-06 15:08:18 +0530

    Upgrade Score card

--- frontend/src/pages/Userpages/FeedBack/RatingScorecard.js
+++ frontend/src/pages/Userpages/FeedBack/RatingScorecard.js
@@ -37,18 +37,18 @@ export default function RatingScorecard({ reviews = [] }) {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
         ))}
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 ml-2">{averageRating.toFixed(2)}</p>
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">out of</p>
-        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-600 ml-2">{averageRating.toFixed(2)}</p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-600">out of</p>
+        <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-600">5</p>
       </div>
-      <p className="text-sm font-medium text-gray-500 dark:text-gray-400">{totalRatings} global ratings</p>
+      <p className="text-sm font-medium text-gray-500 dark:text-gray-600">{totalRatings} global ratings</p>
       {ratingDistribution.map((count, index) => (
         <div key={index} className="flex items-center mt-4">
-          <span className="text-sm font-medium text-gray-700 dark:text-gray-300">{5 - index} star</span>
-          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+          <span className="text-sm font-medium text-gray-400 dark:text-gray-800">{5 - index} star</span>
+          <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-300">
             <div className="h-5 bg-yellow-300 rounded" style={{ width: `${(count / totalRatings) * 100}%` }}></div>
           </div>
-          <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{totalRatings > 0 ? ((count / totalRatings) * 100).toFixed(0) : 0}%</span>
+          <span className="text-sm font-medium text-gray-500 dark:text-gray-800">{totalRatings > 0 ? ((count / totalRatings) * 100).toFixed(0) : 0}%</span>
         </div>
       ))}
     </div>
